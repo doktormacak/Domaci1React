1d25b2a2fb5f45dbebbd8c5debc0c44edba465e1
Conor C. Peterson <conorpetersondev@gmail.com>
2021-12-27 00:59:40 +0100

    Adds state to Form inputs, as well as change and submit handlers

--- registracija/src/components/Form.js
+++ registracija/src/components/Form.js
@@ -1,34 +1,84 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [value, setValue] = useState({
+    username: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    passwordC: "",
+  });
+  const handleChange = (event) => {
+    setValue({ ...value, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("Submitted! ");
+    console.log(event);
+  };
   return (
     <div className="form">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-item">
           <label>Username:</label>
-          <input type="text" name="username" />
-        </div>
-        <div className="form-item">
-          <label>Username:</label>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            value={value.username}
+            placeholder="Username"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-item">
           <label>First name:</label>
-          <input type="text" name="firstname" />
+          <input
+            type="text"
+            name="firstname"
+            value={value.firstname}
+            placeholder="First name"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-item">
           <label>Last name:</label>
-          <input type="text" name="lastname" />
+          <input
+            type="text"
+            name="lastname"
+            value={value.lastname}
+            placeholder="Last name"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-item">
           <label>Email:</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={value.email}
+            placeholder="E-mail"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-item">
           <label>Password:</label>
-          <input type="text" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={value.password}
+            placeholder="Password"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-item">
           <label>Confirm Password:</label>
-          <input type="text" name="passwordC" />
+          <input
+            type="password"
+            name="passwordC"
+            value={value.passwordC}
+            placeholder="Password"
+            onChange={handleChange}
+          />
         </div>
         <input type="submit" value="Submit" />
       </form>

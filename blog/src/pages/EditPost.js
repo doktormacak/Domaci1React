f01b667e785b7ec1075cd023658ad154f319553f
Andrija Oreskovic <76612905+doktormacak@users.noreply.github.com>
2022-01-05 17:04:49 +0100

    Update EditPost.js

--- blog/src/pages/EditPost.js
+++ blog/src/pages/EditPost.js
@@ -1,7 +1,27 @@
+import "./EditPosts.css";
+
 const EditPost = () => {
   return (
-    <div className="">
-      <p>EditPost Page</p>
+    <div className="newPost">
+      <h2>Add a New Blog</h2>
+      <form className="form">
+        <label>Blog title:</label>
+        <input 
+          type="text" 
+          required
+          maxLength={20}/>
+
+        <label>Blog author:</label>
+          <input type="text" maxLength={20}/>
+
+        <label>Image url:</label>
+          <input type="text" />
+        <label>Blog body:</label>
+          <textarea required>
+          </textarea>
+
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };

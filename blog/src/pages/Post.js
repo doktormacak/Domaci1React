f868901d77d9556950c034f6b00a988a757fc49e
Conor C. Peterson <conorpetersondev@gmail.com>
2022-01-05 17:57:02 +0100

    Adds Post.js styling.

--- blog/src/pages/Post.js
+++ blog/src/pages/Post.js
@@ -1,9 +1,35 @@
+// Module imports
 import { useParams } from "react-router";
 
-const Post = () => {
-  const postParams = useParams();
+// Component imports
+import useFetch from "../hooks/useFetch";
 
-  return <h1>Post: {postParams.postId}</h1>;
+//Style imports
+import "./Post.css";
+
+const Post = () => {
+  const params = useParams();
+  const postId = params.postId;
+  const { data, isPending, error } = useFetch(
+    "http://jsonblob.com/928319228822700032"
+  );
+  console.log("data: ", data);
+
+  const postData = data.filter((post) => post.id !== postId);
+
+  console.log("postData: ", postData);
+
+  return (
+    <div className="post-container">
+      <div className="post-header">
+        <h1>Post Title</h1>
+        <p>Post Author</p>
+      </div>
+      <div className="post-body">
+        <p>Post text..</p>
+      </div>
+    </div>
+  );
 };
 
 export default Post;

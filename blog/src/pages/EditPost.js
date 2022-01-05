3883dc8df685f6bbca24a70dc18aeefbf1b7ae0d
Andrija Oreskovic <76612905+doktormacak@users.noreply.github.com>
2022-01-05 18:31:56 +0100

    Update EditPost.js

--- blog/src/pages/EditPost.js
+++ blog/src/pages/EditPost.js
@@ -1,23 +1,58 @@
-import "./EditPosts.css";
+import { useState} from "react";
+
+
+import "./EditPost.css";
 
 const EditPost = () => {
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author,setAuthor] = useState('');
+  const [url, setUrl] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = {title, body , url, author};
+
+    fetch(' https://jsonblob.com/api/jsonBlob/928319228822700032', {
+      method: 'PUT',
+      headers: { "Content-Type" : "application/json", Accept: "application/json"},
+      body: JSON.stringify(blog)
+    })
+  }
+
+
+
   return (
     <div className="newPost">
       <h2>Add a New Blog</h2>
-      <form className="form">
+      <form className="form" onSubmit= {handleSubmit}>
         <label>Blog title:</label>
         <input 
           type="text" 
           required
-          maxLength={20}/>
+          maxLength={20}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}/>
 
         <label>Blog author:</label>
-          <input type="text" maxLength={20}/>
+          <input 
+          type="text"
+          maxLength={20}
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}/>
 
         <label>Image url:</label>
-          <input type="text" />
+          <input 
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)} />
         <label>Blog body:</label>
-          <textarea required>
+          <textarea 
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}>
           </textarea>
 
         <button>Add Blog</button>

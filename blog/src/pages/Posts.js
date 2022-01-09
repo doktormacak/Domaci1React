cc8a7f3a1abad9d8ceac093c83e832a0f8e9441d
Conor C. Peterson <conorpetersondev@gmail.com>
2022-01-09 17:54:57 +0100

    Adds comments to Posts.js

--- blog/src/pages/Posts.js
+++ blog/src/pages/Posts.js
@@ -7,9 +7,14 @@ import PostDisplay from "../components/PostDisplay";
 // Styling imports
 import "./Posts.css";
 
+/**
+ * 'Posts' stranica renderuje kratki prikaz svih postova iz JsonBlob-a.
+ */
 const Posts = () => {
+  // State varijabla koja cuva sve postove ucitane iz bloba
   const [posts, setPosts] = useState([]);
 
+  // UseEffect hook poziva fetch GET request ka JsonBlob-u jednom pri prvom renderu 'Posts' stranice pomocu praznog dependency array-a.
   useEffect(
     () =>
       fetch("https://jsonblob.com/api/jsonBlob/928319228822700032", {
@@ -26,6 +31,9 @@ const Posts = () => {
     []
   );
 
+  // Pomocu .map() metode, 'Posts' poziva 'PostDisplay' komponentu za svaki element(post) unutar 'posts' varijable
+  // i prosljedjuje link ka post-u kao i sam element pomocu props-a.
+  // Ako je duzina 'posts' niza 0, nema postova u blobu.
   return (
     <>
       <div className="posts-container">

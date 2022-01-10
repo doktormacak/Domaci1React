70b7bc0d4ac86405f92d283a8b9a2e374398aa50
Andrija Oreskovic <76612905+doktormacak@users.noreply.github.com>
2022-01-10 14:30:40 +0100

    Update PostDisplay.js

--- blog/src/components/PostDisplay.js
+++ blog/src/components/PostDisplay.js
@@ -13,7 +13,7 @@ const PostDisplay = (props) => {
       <div className="col s12 m4 l4">
         <div className="card">
           <div className="card-image">
-            <img src={require('./img.jpg')} alt="img"/>
+            <img src= {props.post.url} alt="img"/>
             <span className="card-title">{ props.post.title }</span>
           </div>
           <div className="card-content">
